c9ae13dbe118367c97965decc5ba32eabf2c05c1
Alexey Zauzin <lehadnk@gmail.com>
2019-12-21 15:02:55 +0800

    Fixed a bug

--- run.ts
+++ run.ts
@@ -18,14 +18,14 @@ discordClient.on("message", async msg => {
     // @ts-ignore
     channel = msg.channel;
 
-    if (msg.content === '!предсказание' && !locker.isLockedForMember(msg.author.id)) {
+    if (msg.content === '!предсказание' && !locker.isLockedForMember(msg.guild.id, msg.author.id)) {
         msg.channel.send("<@"+msg.author.id+"> "+phrasesGenerator.getOne(msg.author.id)).catch(() => {
             console.log("No msg send permissions for "+channel.name+" in "+msg.guild.name);
         });
         msg.delete(0).catch(() => {
             console.log("No msg delete permissions for "+channel.name+" in "+msg.guild.name);
         });
-        locker.lockForMember(msg.author.id);
+        locker.lockForMember(msg.guild.id, msg.author.id);
     }
 });
 discordClient.login(process.env.DISCORD_BOT_TOKEN);
